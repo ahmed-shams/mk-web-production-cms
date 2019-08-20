b6427af35fe9ccb42612cc72c2f8dc9d38ccd273
Sehwan Choi <sehwan.choi@michaelkors.com>
2019-08-20 11:49:54 -0400

    Add signup page UI and route

--- client/pages/user/login.js
+++ client/pages/user/login.js
@@ -1,9 +1,11 @@
 import React, { useState, useCallback } from 'react';
+import Link from 'next/link';
 import { Input, Button, Form } from 'antd';
+import styled from 'styled-components';
 
 
 // custom hook for making useState easier
-const useInput = (initialValue = null) => {
+export const useInput = (initialValue = null) => {
   const [value, setter] = useState(initialValue);
   const handler = useCallback(
     (e) => { 
@@ -15,26 +17,46 @@ const useInput = (initialValue = null) => {
 
 const Login = () => {
   const [id, onChangeId] = useInput('');
-  const [password, onChangePassowrd] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log({id, password});
+  }, [id, password])
 
   return (
-    <Form>
-      <div>
-        <label htmlFor='user-id'>id</label>
-        <br />
-        <input name='user-id' value={id} onChange={onChangeId} required />
-      </div>
-      <div>
-        <label htmlFor='user-password'>id</label>
-        <br />
-        <input name='user-password' value={password} onChange={onChangePassword} required />
-      </div>
-      <div style={{marginTop: '10px'}}>
-        <Button type='primary' htmlType='submit'>Submit</Button>
-        <Link href='/signup'><a><Button>Sign Up</Button></a></Link>
-      </div>
-    </Form>
+    <LoginContainer>
+      <Form onSubmit={onSubmitForm} styled={{ padding: '10px' }}>
+        <div>
+          <label htmlFor='user-id'>id</label>
+          <br />
+          <Input name='user-id' value={id} onChange={onChangeId} required />
+        </div>
+        <div>
+          <label htmlFor='user-password'>Password</label>
+          <br />
+          <Input name='user-password' value={password} onChange={onChangePassword} required />
+        </div>
+        <div style={{marginTop: '10px'}}>
+          <Button type='primary' htmlType='submit'>Login</Button>
+          <Link href='/signup'><a><Button>Sign Up</Button></a></Link>
+        </div>
+      </Form>
+    </LoginContainer>
   )
 };
 
 export default Login;
+
+const LoginContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
