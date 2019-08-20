3ea260847d8f966784d3de8e209c29ebf6323f50
Sehwan Choi <sehwan.choi@michaelkors.com>
2019-08-20 16:48:45 -0400

    Add error page

---
+++ client/pages/_error.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error has occurred</h1>
+      <div>
+        <h2>Useful Resources & Recommended Page</h2>
+        <ul>
+          <li><a href="www.google.com" target="_blank">Google</a></li>
+          <li><a href="www.facebook.com" target="_blank">Facebook</a></li>
+          <li><a href="www.amazon.com" target="_blank">Amazon</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
