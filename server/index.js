8bed23c3bb266fd2c006a25442a239bf56def5b0
Sehwan Choi <sehwan.choi@michaelkors.com>
2019-08-22 17:18:03 -0400

    Create db by syncing sequelize

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const cors = require('cors'); 
+const morgan = require('morgan');
+
+// DB
+const db = require('./models');
+
+
+// app
 const app = express();
+db.sequelize.sync();
+
+app.use(morgan('dev'));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send("HOME MAIN PAGE");
