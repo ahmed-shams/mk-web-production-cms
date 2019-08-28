96b0a7a60707f137f1e7a048af2e358aa07ee4e6
Sehwan Choi <sehwan.choi@michaelkors.com>
2019-08-28 15:35:47 -0400

    Update add new file + dummy file

--- client/pages/add_new.js
+++ client/pages/add_new.js
@@ -1,60 +1,111 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { Form, Input, Button } from 'antd';
-import { useInput } from './user/login';
-import styled from 'styled-components';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOAD_ALL_FILE_REQUEST } from '../reducers/file';
+import { Treebeard } from 'react-treebeard';
+import { Layout, Form, Input, Button, Radio } from 'antd';
+const { Content, Sider } = Layout;
+const { TextArea } = Input; 
 
 const NewFile = () => {
-  const [filename, onChangeFilename] = useInput('');
-  const [json, onChangeJson] = useInput('');
-  const [category, onChangeCategory] = useInput('');
   const dispatch = useDispatch();
-
-  const onSubmitHandler = useCallback((e) => {
-    e.preventDefault();
+  const { Files } = useSelector(state => state.file);
+  const [data, setData] = useState({});
+  const [cursor, setCursor] = useState(false);
+  const [importMode, setImportMode] = useState('json');
+  const [filename, setFilename] = useState('');
+  const [fileContent, setFileContent] = useState('');
+  
+  useEffect(() => {
     dispatch({
-      type: ADD_POST_REQUEST,
-      data: {
-        filename,
-        json,
-        category
-      }
+      type: LOAD_ALL_FILE_REQUEST
     })
-  }, [filename, json, category]);
+    setData(Files);
+  }, [Files])
+
+  const onToggle = (node, toggled) => {
+    if (cursor) {
+      cursor.active = false;
+    }
+    node.active = true;
+    if (node.children) {
+      node.toggled = toggled;
+    }
+    setCursor(node);
+    setData(Object.assign({}, data))
+
+
+    if (importMode === 'json') {
+      alert("json mode..")
+    } else if (importMode === 'import') {
+      setFilename(node.name);
+      setFileContent(JSON.stringify(node, null, 4));
+    }
+    
+
+    // setJson(node);
+    // dispatch({
+    //   type: GET_A_FILE_REQUEST,
+    //   data: node.id
+    // })
+  }
+  
+  const onChangeFileName = useCallback((e) => {
+    setFilename(e.target.value);
+  });
+
+  const onChangeFileContent = useCallback((e) => {
+    setFileContent(e.target.value);
+  });
+
+  const onChangeRadio = useCallback((e) => {
+    alert(e.target.value);
+    setImportMode(e.target.value);
+  })
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    alert(e.target)
+  }
+
+  const copyText = () => {
+    navigator.clipboard.writeText(fileContent).then(()=>{
+      alert('Copying to clipboard was successful');
+    }, (e) => {
+      alert('error happened while trying to copy josn. please try again');
+    })
+  }
 
   return (
-    <Container>
-      <Form onSubmit={onSubmitHandler} id="text-form">
-        <div>
-          <label htmlFor='filename' >File name: </label>
-          <Input name='filename' value={filename} onChange={onChangeFilename} />
-        </div>
-        <div>
-          <label htmlFor='json'>CONTENT</label>
-          <br />
-          <Input name='json' value={json} onChange={onChangeJson} />
-        </div>
-
-        <div>
-          <label htmlFor='category'>Category</label>
-          <br />
-          <Input name='category' value={category} onChange={onChangeCategory} />
-        </div>
-        <div style={{marginTop: '15px'}}>
-          <Button type='primary' htmlType='submit'>Save</Button>
-          <Button>Preview</Button>
-        </div>
-      </Form>
-    </Container>
-  )
+    <div>
+      <h1>All Files</h1>      
+      <Layout>
+        <Sider>
+          {Files && <Treebeard data={data} onToggle={onToggle} />}
+        </Sider>
+        <Content style={{padding:'20px'}}>
+          <h1>File Content</h1>
+          <Form onSubmit={onSubmitHandler}>
+            <label><strong>File Name</strong></label>
+            <Input value={filename} onChange={onChangeFileName} />
+            <label><strong>Content</strong></label>
+            <div style={{padding: '10px 0'}}>
+              <Radio.Group defaultValue="a" buttonStyle="solid" onChange={onChangeRadio}>
+                <Radio.Button value="json">JSON MODE</Radio.Button>
+                <Radio.Button value="import">Import Mode</Radio.Button>
+              </Radio.Group>
+            </div>
+            <TextArea row={50} value={fileContent} onChange={onChangeFileContent} style={{minHeight: '500px'}} />
+            <Button type='primary' style={{marginRight: '10px'}}>Preview</Button>
+            <Button type='danger' style={{marginRight: '10px'}} onClick={copyText} >Copy JSON</Button>
+            <Button htmlType='submit'>Save</Button>
+          </Form>
+        </Content>
+        <Sider>
+          <h2 style={{color: "white", textAlign: "center"}}>Create a category</h2>
+        </Sider>
+      </Layout>
+    </div>
+  );
 };
 
 export default NewFile;
-
-const Container = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
--- client/reducers/file.js
+++ client/reducers/file.js
@@ -19,6 +19,7 @@ const dummyFile = {
   "name": "Root-FRC-Folder",
   "toggled": false,
   "userId": 1,
+  "content": [],
   "children": [
     {
       "name": "EN_US",
@@ -67,8 +68,168 @@ const dummyFile = {
               "userId": 2
             },
             {
-              "name": "Michael's World.js",
-              "userId": 1
+              "name": "16x9-banner.js",
+              "userId": 1,
+              "content": [{
+                "componentId": "banner16by9",
+                "globalOptions": {
+                  "data_linkcontainer": "PROMO 1",
+                  "personalization": "personalized id",
+                  "gutters": false,
+                  "fullbleed": false,
+                  "padding": {
+                    "top": "",
+                    "bottom": "ext-padding-100"
+                  },
+                  "background": {
+                    "image": {
+                      "src": "",
+                      "repeat": "none",
+                      "position": "",
+                      "size": "cover"
+                    },
+                    "color": "#FFFFFF",
+                    "inner_color": "blue"
+                  },
+                  "hide_mobile": false,
+                  "hide_desktop": false,
+                  "hide_tablet": false
+                },
+                "imageOptions": {
+                  "imgSrc": {
+                    "mobile": "https://digital1.michaelkors.com/refreshes/2019/spring/refresh4/global/mobile/homepage/HP_PROMO_1.jpg",
+                    "tablet": "https://digital1.michaelkors.com/refreshes/2019/spring/refresh4/global/desktop/homepage/HP_PROMO_1.jpg",
+                    "desktop": "https://digital1.michaelkors.com/refreshes/2019/spring/refresh4/global/desktop/homepage/HP_PROMO_1.jpg",
+                    "fallback": "https://digital.michaelkors.com/refreshes/2019/spring/refresh1/global/mobile/homepage_1-29/HP_PROMO_1.jpg",
+                    "alt": "test image"
+                  },
+                  "imgInfo": {
+                    "link_url": "/women/_/N-28ee",
+                    "data_icid": "FEEL THE LOVE",
+                    "tabindex": -1,
+                    "aria": {
+                      "hidden": true,
+                      "label": "test hello"
+                    }
+                  },
+                  "lazyload": false,
+                  "disclaimer": {
+                    "html": true,
+                    "text": "<span>This is a disclaimer text - <a href='www.google.com'>click here for detail</a></span>",
+                    "text_align": "center"
+                  }
+                },
+                "textOptions": {
+                  "type_wrapper": {
+                    "padding": {
+                      "top": "",
+                      "bottom": "int-padding-30"
+                          },
+                          "caption_width_desktop": "75%",
+                          "caption_width_tablet": "80%",
+                          "caption_width_mobile": "90%",
+                    "text_align": "center",
+                          "mobile_left_to_center": false,
+                          "color": "red"
+                  },
+                  "slug": {
+                    "text": "Lorem Ipsum",
+                    "fontfamily": "type-slug-eu",
+                    "link": "http://www.google.com/",
+                    "data_icid": "headline data icid",
+                    "tabindex": -1,
+                    "aria_hidden": true,
+                    "aria_label": "test hello",
+                    "padding": {
+                      "top": "int-padding-30",
+                      "bottom": "int-padding-30"
+                    }
+                  },
+                  "typedivider": true,
+                  "texts": [{
+                      "classprop": "wpHeadline",
+                      "text": "50% Width, Whiteny-Caslon Type Style",
+                      "fontfamily": "type-headline-h2-large",
+                      "link": "http://www.google.com/",
+                      "tabindex": -1,
+                      "aria_hidden": true,
+                      "aria_label": "test hello",
+                      "data_icid": "headline data icid",
+                      "padding": {
+                        "top": "",
+                        "bottom": "int-padding-30"
+                      },
+                      "svg": {
+                        "src_desktop": "https://digital.michaelkors.com/sales/2019/test/25_OFF_SALE_desktop.svg",
+                        "src_mobile": "https://digital.michaelkors.com/sales/2019/test/25_OFF_SALE_mobile.svg",
+                        "alt": "Buy Buy Buy",
+                        "width_mobile": "300px",
+                        "width_tablet": "80vw",
+                        "width_desktop_s": "700px",
+                        "width_desktop_l": "80%"
+                      }
+                    },
+                    {
+                      "classprop": "wpText",
+                      "text": "Bus ac tus; nostis lina, nicapeciem. Vala es! Serit, se mussenatia ta se nihi, denatid serem morte inercerfinat fordiem, in Itam cut furo notiliam intiliam ocum P. Sentiam satabem conoret vivilic iistiae ad consusp eculles intiam.",
+                      "textTablet": "Hello Tablet Text Here",
+                      "textMobile": "Hello Mobile Text Here",
+                      "fontfamily": "type-copy-caslon-deck",
+                      "link": "http://www.google.com/maps",
+                      "tabindex": -1,
+                      "aria_hidden": false,
+                      "aria_label": "test aria 2",
+                      "data_icid": "body copy data_icid",
+                      "padding": {
+                        "top": "",
+                        "bottom": "int-padding-30"
+                      },
+                      "color":"blue"
+                    },
+                    {
+                      "classprop": "wpText",
+                      "text": "Bus ac tus; nostis lina, nicapeciem. Vala es! Serit, se mussenatia ta se nihi, denatid serem morte inercerfinat fordiem, in Itam cut furo notiliam intiliam ocum P. Sentiam satabem conoret vivilic iistiae ad consusp eculles intiam.",
+                      "fontfamily": "type-copy-caslon-deck",
+                      "link": "http://www.google.com/maps",
+                      "tabindex": -1,
+                      "aria_hidden": false,
+                      "aria_label": "test hello 3",
+                      "data_icid": "body copy data_icid",
+                      "padding": {
+                        "top": "",
+                        "bottom": "int-padding-30"
+                      },
+                      "color":"#2980b9"
+                    }
+                  ],
+                  "ctaDefault": {
+                    "ctas": [{
+                      "text": "SHOP MEN'S NEW ARRIVALS",
+                      "show_mobile_only": false,
+                      "link": "http://www.google.com",
+                      "data_icid": "SHOP NOW Data icid",
+                      "aria_label": "SHOP NOW",
+                      "padding": {
+                        "top": "",
+                        "bottom": "int-padding-30"
+                      }
+                    },
+                    {
+                      "text": "SHOP WOMEN",
+                      "show_mobile_only": false,
+                      "link": "http://www.reddit.com",
+                      "data_icid": "women data icid test",
+                      "aria_label": "shop there now",
+                      "padding": {
+                        "top": "",
+                        "bottom": ""
+                      }
+                    }],
+                    "isEU": false,
+                    "color": "black"
+                  }
+                }
+              }]
             }
           ]
         },
