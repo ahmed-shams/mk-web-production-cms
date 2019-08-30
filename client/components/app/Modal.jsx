e380bfc27e2eab0cb032cc6c870e3b45987728e2
Eric Forsell <eric.forsell@michaelkors.com>
2019-08-30 14:41:20 -0400

    All component renderJson

--- client/components/app/Modal.jsx
+++ client/components/app/Modal.jsx
@@ -79,18 +79,100 @@ const PreviewContent = styled.div`
 `
 
 
-const renderJSONComponents = (dataObj) => {
-  const compsArr = [];
-  console.log('dataObj ', dataObj);
-  if(!dataObj[0]) return;
-    switch (dataObj[0].componentId) {
-      case 'banner16by9':
-        compsArr.push(<DefaultBanner data={dataObj[0]} key="1" contained />);
+const renderJSONComponents = (dataObj, isMobile, key, ref) => {
+    const compsArr = [];
+    dataObj.forEach((compObj, i) => {
+      if (key) i = key + i;
+      switch (compObj.componentId) {
+        case 'banner16by9':
+          compsArr.push(<DefaultBanner data={compObj} key={i} contained />);
+          break;
+        case 'arrowButton':
+          compsArr.push(<ArrowButton data={compObj} key={i} />);
+          break;
+        case 'closeButton':
+          compsArr.push(<CloseButton data={compObj} key={i} />);
+          break;
+        case 'resize':
+          compsArr.push(<Resize data={compObj} key={i} />);
+          break;
+        case 'countdown':
+          compsArr.push(<Countdown data={compObj} key={i} />);
+          break;
+        case 'modal':
+          compsArr.push(<Modal data={compObj} isMobile={isMobile} key={i} />);
+          break;
+        case 'grid':
+          compsArr.push(<Grid data={compObj} isMobile={isMobile} key={i} />);
+          break;
+        case 'lookback':
+          compsArr.push(<Lookback data={compObj} isMobile={isMobile} key={i} />);
+          break;
+        case 'carousel':
+          compsArr.push(<Carousel data={compObj} ref={ref} isMobile={isMobile} key={i} />);
+          break;
+        case 'columns':
+          compsArr.push(<Columns data={compObj} isMobile={isMobile} key={i} />);
+          break;
+        case 'youTubeVideo':
+          compsArr.push(<YouTubeVideoPlay data={compObj} key={i} />);
+          break;
+        case 'htmlVideo':
+          compsArr.push(<HtmlVideoPlay data={compObj} key={i} />);
         break;
-      default:
-    }
-  return compsArr;
-}
+        case 'video4by5':
+          compsArr.push(<Video4by5 data={compObj} key={i} />);
+          break;
+        case 'testMockData':
+          compsArr.push(<TestMockData data={compObj} isMobile={isMobile} key={i} />);
+          break;
+        case 'banner4by5':
+          compsArr.push(<DefaultBanner data={compObj} key={i} contained />);
+          break;
+        case 'toi':
+          compsArr.push(<DefaultBanner data={compObj} key={i} contained />);
+          break;
+        case 'parallax4by5EvenLeft':
+          compsArr.push(<Parallax4by5EvenLeft data={compObj} key={i} />);
+          break;
+        case 'parallaxFixedParagraph':
+          compsArr.push(<ParallaxFixedParagraph data={compObj} key={i} />);
+          break;
+        case 'parallaxFixedBackground':
+          compsArr.push(<ParallaxFixedBackground data={compObj} key={i} />);
+          break;
+        case 'simpleGallery':
+          compsArr.push(<SimpleGallery sliderIndex={i} data={compObj} key={i} />);
+          break;
+        case 'pageDivider':
+          compsArr.push(<PageDivider data={compObj} key={i} contained />);
+          break;
+        case 'dk-navigation':
+          compsArr.push(<DkNavigation data={compObj} key={i} />);
+          break;
+        case 'giftServices':
+          compsArr.push(<GiftServices data={compObj} key={i} />);
+          break;
+        case 'topBanner':
+          compsArr.push(<TopBanner data={compObj} key={i} />);
+          break;
+        case 'topBannerCarousel':
+          compsArr.push(<TopBannerCarousel data={compObj} key={i} />);
+          break;
+        case 'mwRunway':
+          compsArr.push(<MwRunway data={compObj} key={i} />);
+          break;
+        case 'mwTravelDiaries':
+          compsArr.push(<MwTravelDiaries data={compObj} key={i} />);
+          break;
+        case 'lookBook':
+          compsArr.push(<LookBook data={compObj} key={i} />);
+          break;
+        default:
+      }
+    });
+    return compsArr;
+  }
 
 
 const Modal = ({ show = false, onClose, fileJson }) => {
