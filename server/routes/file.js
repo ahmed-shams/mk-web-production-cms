f0520ca8eeb6e5dc8b3cd966b3db5088f359f90a
Giuseppe Stramandinoli <Giuseppe.Stramandinoli@MichaelKors.com>
2019-09-03 11:38:13 -0400

    Added method to update file

--- server/routes/file.js
+++ server/routes/file.js
@@ -24,4 +24,34 @@ router.post('/', async (req, res, next) => { // Sign up
   }
 });
 
+// request body parameters: userId, fileId, content
+// response body parameters: entriesUpdated
+router.put('/', async (req, res, next) => {
+	try {
+		// Get old file content from DB 
+		const oldContent = await db.File.findOne({
+			where: { id: req.body.fileId
+			}
+		});
+		
+		// Insert old content into Revisions
+		const revisionInsert = await db.Revision.create({
+			  content: oldContent.content,
+			  name: oldContent.name,
+			  UserId: req.body.userId,
+			  fileId: req.body.fileId
+			});
+		
+		// Insert New Content into Files
+		const newFile = await db.File.update(
+		{content: req.body.content}, 
+		{where: {Id: req.body.fileId}});
+
+		return res.status(200).json({'entriesUpdated': newFile[0]});
+	} catch (e) {
+		console.error(e);
+		return next(e);
+	}
+}); 
+
 module.exports = router;
