61c9a581f21f980cde48df577ecc6d205b822055
Eric Forsell <eric.forsell@michaelkors.com>
2019-09-14 23:54:31 -0400

    Add basic header to preview modal

--- client/components/app/Modal.jsx
+++ client/components/app/Modal.jsx
@@ -39,18 +39,63 @@ export const ModalContainer = styled.div`
 export const ModalHeader= styled.div`
   display: flex;
    justify-content: center;
-  align-items: center;
+   flex-direction: column;
+   border-bottom: 1px solid lightgrey;
   font-size: 45px;
   font-weight: bold;
-  background-color: black;
-  color: white;
+  background-color: white;
+  color: black;
   align-items: center;
   position: absolute;
   top: 0;
   width: 100%;
   left: 0;
   height: 200px;
-`
+`;
+
+export const ModalHeaderTop= styled.div`
+  display: flex;
+   justify-content: center;
+  font-size: 45px;
+  font-weight: bold;
+  color: black;
+  height: 100px;
+  align-items: flex-end;
+  width: 100%;
+`;
+
+export const ModalHeaderBottom= styled.div`
+  display: flex;
+   justify-content: center;
+  font-size: 45px;
+  font-weight: bold;
+  height: 100px;
+  color: white;
+  align-items: flex-end;
+  width: 100%;
+`;
+
+
+export const ModalHeaderNav= styled.ul`
+  display: flex; 
+  justify-content: center;
+  align-items:flex-end;
+  color: white;
+  margin: 0;
+  width: 75%;
+`;
+
+export const ModalHeaderNavItem= styled.li`
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  color: black;
+  width: 11%;
+  font-size: 14px;
+  height: 60px;
+`;
+
+
 
 const CopyHtml = styled.button`
   position: absolute;
@@ -62,8 +107,8 @@ const CopyHtml = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: white;
-  color: black;
+  background-color: black;
+  color: white;
   border-radius: 5px;
     &:hover {
     cursor: pointer;
@@ -79,7 +124,7 @@ export const ModalClose = styled.div`
   align-items: center;
   border: none;
   font-size: 50px;
-  color: white;
+  color: black;
 
   &:hover {
     cursor: pointer;
@@ -224,7 +269,42 @@ const ModalPreview = ({ onClose, fileJson }) => {
   <div>  
   	<ModalContainer>
       <CopyHtml onClick={copyHtml}>COPY HTML</CopyHtml>
-      <ModalHeader>PREVIEW</ModalHeader>
+      <ModalHeader>
+        <ModalHeaderTop>
+          MICHAEL KORS
+        </ModalHeaderTop>
+        <ModalHeaderBottom>
+          <ModalHeaderNav>
+            <ModalHeaderNavItem>
+              WOMEN
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              MEN
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              COLLECTION
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              HANDBAGS
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              WALLETS
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              SHOES
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+             WATCHES
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              GIFTS
+            </ModalHeaderNavItem>
+            <ModalHeaderNavItem>
+              SALE
+            </ModalHeaderNavItem>
+          </ModalHeaderNav>
+        </ModalHeaderBottom>
+      </ModalHeader>
       <ModalClose onClick={onClose}>X</ModalClose>
       <PreviewContent ref={ContentContainer}>{json}</PreviewContent>
     </ModalContainer>
