3d9f73bc8726330ef2602a4721d5614ccb7e365c
Sehwan Choi <sehwan.choi@michaelkors.com>
2019-09-14 21:34:02 -0400

    Update meta field using helmet

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -15,12 +15,37 @@ import '../utils/fake.css';
 const Layout = ({ Component, store, pageProps }) => {
   return (
     <Provider store={store}>
-      <Head>
-        <title>MK WEB CMS</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.20.1/antd.css" />
-        <link rel="stylesheet" href="https://www.michaelkors.com/img/MKBusiness/css/2019/fall/refresh2/mk-style.min.css" />
-        <link rel="stylesheet" href="node_modules/foundation-sites/dist/css/foundation-sites.min.css" />
-      </Head>
+      <Helmet
+        title="MK Web Production App"
+        htmlAttributes={{ lang: 'en' }}
+        meta={[{
+          charset: 'UTF-8',
+        }, {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
+        }, {
+          'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
+        }, {
+          name: 'description', content: 'Michael Kors Web Prodcution Internal App',
+        }, {
+          name: 'og:title', content: 'MK Web Production App',
+        }, {
+          name: 'og:description', content: 'Michael Kors Web Prodcution Internal App',
+        }, {
+          property: 'og:type', content: 'website',
+        }, {
+          property: 'og:image', content: 'https://digital.michaelkors.com/refreshes/2019/fall/refresh2/global/desktop/women/LP_WOMEN_1.jpg',
+        }]}
+        link={[{
+          rel: 'shortcut icon', href: '/favicon.ico',
+        }, {
+          rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css',
+        }, {
+          rel: 'stylesheet', href: 'https://www.michaelkors.com/img/MKBusiness/css/2019/fall/refresh2/mk-style.min.css',
+        }, {
+          rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
+        }]}
+      />
       <AppLayout>
         <Component {...pageProps} />
       </AppLayout>
